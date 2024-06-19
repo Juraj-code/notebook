b7f0b1620538cf3ce9017410da4afcce66a67da3
Juraj Palko <juraj.palko@dxc.com>
2024-06-19 13:18:58 +0200

    implement login, logout, signup add templates, bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,36 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 # import logging
 # from logging import RotationgFileHandler, SMTPHandler
 
 # TODO: Implementuj logovanie
+# TODO: Prerob na Factory
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
+app.secret_key = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
-## Globálna premenná na sledovanie inicializácie
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app.models import User
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+bcrypt = Bcrypt(app)
+
+
+## Globálna premenná na sledovanie inicializácie databazy
 initialization_done = False
     
 ## Inicializacia databazy 
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,71 @@
-from flask import jsonify
-from app import app, db
+from flask import jsonify, render_template, request, Flask, url_for, redirect
+from app import app, db, bcrypt
 from app.models import Person
+from app.models import User
+from flask_login import login_user, logout_user, current_user, login_required
+
 
 @app.route('/')
 def index():
-    return 'Work in progress!'
+    return render_template('index.html')
 
+## Test of connection to the database
 @app.route('/test_db_connection')
 def test_db_connection():
     try:
-        person_count = db.session.query(Person).count()
-        return jsonify(success=True, message="Database connection successful", person_count=person_count)
+        people_count = db.session.query(Person).count()
+        return jsonify(success=True, message="Database connection successful", people_count=people_count)
     except Exception as e:
         return jsonify(success=False, message=str(e))
+    
+# TODO: Limit na userov v databaze
+# TODO: Exceptions    
+## Registration of the user
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        
+        user = User(username=username, password=hashed_password)
+        
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+## Login 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        user = User.query.filter(User.username == username).first()
+        print("user :"+ str(user.username))
+        
+        if user is None:
+            return jsonify(message="Invalid username or password"), 401
+    
+        if bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return render_template('index.html')
+        else:
+            return 'Failed'
+    
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/secret')
+@login_required
+def secret():
+    return 'The Secrete'
+    
